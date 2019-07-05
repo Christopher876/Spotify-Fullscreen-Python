e728ae3464f9bee9a85e52c98812a192b9f47249
Christopher WIlliams <christopher876@outlook.com>
2019-07-05 15:33:07 -0400

    Initial Commit -Add the project to github -Add all setup for the project -Add QT5 interface -Add Spotify Class that controls the spotify interaction

---
+++ full-spotify.py
@@ -0,0 +1,124 @@
+import sys
+from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDialog
+from PyQt5.QtGui import QIcon, QPixmap, QIcon
+import spotipy
+from spotify_account import Spotify
+import threading
+import time
+
+class Login_Dialog(QDialog):
+    def __init__(self):
+        super(Login_Dialog,self).__init__()
+        loadUi('spotify-dialog.ui',self)
+        self.setWindowTitle("Spotify Account")
+        
+        self.options_button.accepted.connect(self.save_login_info)
+    
+    #TODO NOT WORKING ATM
+    @pyqtSlot()    
+    def save_login_info(self):
+        '''with open("config.ini","w") as outfile:
+            outfile.write(self.username.toPlainText() + '\n')
+            outfile.write(self.client_id.toPlainText() + '\n')
+            outfile.write(self.client_secret.toPlainText())
+'''
+class Fullscreen_Spotify(QMainWindow):
+    def __init__(self):
+        super(Fullscreen_Spotify,self).__init__()
+        loadUi('spotify-fullscreen.ui',self)
+        self.setWindowTitle("Full Screen Spotify")
+        self.setWindowIcon(QIcon('icon.png'))
+        self.showFullScreen()
+        #self.test1.clicked.connect(self.on_click)
+
+        self.menu_account.triggered.connect(self.account_login)
+
+        self.spotify = Spotify()
+        self.set_widgets_song_info()
+        self.menubar.setVisible(False)
+
+        self.need_to_exit = False
+        self.check_thread = threading.Thread(target=self.check_song_update)
+        self.check_thread.start()
+    
+    #Check if the song has changed and then change it
+    def check_song_update(self):
+        while(self.need_to_exit != True):
+            time.sleep(2)
+            self.set_widgets_song_info()
+    
+    #Set all the information from the now playing song to the QT interface
+    def set_widgets_song_info(self):
+        song_info = self.spotify.get_song_info()
+        
+        if song_info is not None and song_info['song_name'] != self.song_name.text(): 
+            #Set new album info
+            pixmap = QPixmap()
+            pixmap.loadFromData(self.spotify.get_album_art(song_info['album_art']))
+            self.image1.setPixmap(pixmap)
+            self.image1.resize(1920,1080)
+
+            self.album_name.setText(song_info['album_name'])
+            self.artist_name.setText(song_info['artist_name'])
+            self.release_date.setText(song_info['release_date'])
+            self.song_length.setText(song_info['song_length'])
+            self.song_name.setText(song_info['song_name'])
+
+    #Setup for getting key events
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == Qt.Key_Escape:
+            self.need_to_exit = True
+            time.sleep(1)
+            QApplication.exit()
+        
+        if key == Qt.Key_Right:
+            self.spotify.next_track()
+            self.set_widgets_song_info()
+        
+        if key == Qt.Key_Left:
+            self.spotify.previous_track()
+            self.set_widgets_song_info()
+        
+        if key == Qt.Key_Space:
+            if self.spotify.is_playing_music is True:
+                self.spotify.pause_track()
+            else:
+                self.spotify.play_track()
+
+        if key == Qt.Key_M:
+            if self.menubar.isVisible():
+                self.menubar.setVisible(False)
+            else:
+                self.menubar.setVisible(True)
+        
+        if key == Qt.Key_R:
+            self.set_widgets_song_info()
+        
+        if key == Qt.Key_I:
+            if self.isFullScreen():
+                self.showMaximized()
+            else:
+                self.showFullScreen()
+
+    @pyqtSlot()
+    def on_click(self):
+        pass
+    
+    @pyqtSlot()
+    #Show the dialog box for the account info
+    def account_login(self):
+        spotify_account = Login_Dialog()
+        spotify_account.exec_()
+
+def main():
+    app = QApplication(sys.argv)
+    widget = Fullscreen_Spotify()
+    widget.show()
+    sys.exit(app.exec_())
+    print("Exited Successfully!")
+
+if __name__ == "__main__":
+    main()
---
+++ spotify_account.py
@@ -0,0 +1,100 @@
+import spotipy
+import spotipy.util as util
+from urllib import request
+import pprint
+import math
+from configparser import ConfigParser
+import os
+
+pp = pprint.PrettyPrinter(indent=4)
+
+class Spotify_Device():
+    id : str
+    name : str
+    is_active : bool
+    is_restricted : bool
+
+    def __init__(self,id=None,name=None,is_active=None, is_restricted=None):
+        self.id = id
+        self.name = name
+        self.is_active = is_active
+        self.is_restricted =  is_restricted
+
+class Spotify:
+    is_playing_music : bool
+    
+    def __init__(self):
+        config = ConfigParser()
+        if os.path.isfile('config.ini'):
+            config.read('config.ini',encoding='utf-8-sig')
+
+        self.token = util.prompt_for_user_token(config['SPOTIFY']['username'],'user-read-playback-state',config['SPOTIFY']['client_id'],config['SPOTIFY']['client_secret'],config['SPOTIFY']['redirect_uri'])
+        
+        if self.token:
+            #Spotify Login
+            self.sp = spotipy.Spotify(auth=self.token)
+            self.sp.trace = False
+            self.login_successful = True
+            self.active_device = Spotify_Device()
+        else:
+            self.login_successful = False
+
+    def get_devices(self):
+        return self.sp.devices()
+    
+    def get_active_device(self, devices):
+        self.devices = []
+        for device in devices['devices']:
+            if device['is_active'] is True:
+                self.active_device = Spotify_Device(id = device['id'], name = device['name'], is_active = device['is_active'], is_restricted = device['is_restricted'])
+            self.devices.append(Spotify_Device(id = device['id'], name = device['name'], is_active = device['is_active'], is_restricted = device['is_restricted']))
+
+
+    def get_album_art(self, album_url):
+        return request.urlopen(album_url).read()
+    
+    #Playback Controls
+    def next_track(self):
+        self.sp.next_track(device_id=self.active_device.id)
+    
+    def previous_track(self):
+        self.sp.previous_track(device_id=self.active_device.id)
+    
+    def pause_track(self):
+        self.sp.pause_playback(device_id=self.active_device.id)
+        self.is_playing_music = False
+    
+    def play_track(self):
+        self.sp.start_playback(device_id=self.active_device.id)
+        self.is_playing_music = True
+            
+    def get_song_info(self):
+        try:
+            current_song = self.sp.current_user_playing_track()
+            if current_song is not None:
+                self.is_playing_music = True
+                song_info = current_song['item']
+                album_info = current_song['item']['album']
+
+                artist_name = album_info['artists'][0]['name']
+                album_name = album_info['name']
+                release_date = album_info['release_date']
+                biggest_album_image = album_info['images'][0]['url']
+                song_name = song_info['name']
+                song_length_seconds = int(int(song_info['duration_ms']) / 1000)
+                song_popularity = int(song_info['popularity'])
+
+                return {
+                    'artist_name' : artist_name, 
+                    'album_name' : album_name,
+                    'release_date' : release_date,
+                    'album_art' : biggest_album_image,
+                    'song_name' : song_name,
+                    'song_length' : str(math.floor(song_length_seconds/60)) + ':' + str(song_length_seconds%60),
+                    'song_popularity' : song_popularity}
+            else:
+                self.is_playing_music = False
+                print("No music is playing")
+        except Exception:
+            self.sp = spotipy.Spotify(auth=self.token)
+            self.get_song_info()
